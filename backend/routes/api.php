ec6742b694220edfcbdb6abddb75109f181888dd
simiyu-samuel <simiyusamuel869@gmail.com>
2026-05-02 14:25:56 +0300

    refactor: modularize API routes into routes/api/modules

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,20 +1,18 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// Public Auth Routes
-Route::post('/auth/register', [AuthController::class, 'register']);
-Route::post('/auth/login', [AuthController::class, 'login']);
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
 
-// Protected Routes
-Route::middleware('auth:sanctum')->group(function () {
-    // User Profile
-    Route::get('/user', [AuthController::class, 'user']);
-    Route::post('/auth/logout', [AuthController::class, 'logout']);
-    
-    Route::put('/user/profile', [ProfileController::class, 'update']);
-    Route::post('/user/avatar', [ProfileController::class, 'uploadAvatar']);
-    Route::post('/user/banner', [ProfileController::class, 'uploadBanner']);
-});
+// Modular Routes
+require __DIR__ . '/api/modules/auth.php';
+require __DIR__ . '/api/modules/user.php';
---
+++ backend/routes/api/modules/user.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::put('/user/profile', [ProfileController::class, 'update']);
+    Route::post('/user/avatar', [ProfileController::class, 'uploadAvatar']);
+    Route::post('/user/banner', [ProfileController::class, 'uploadBanner']);
+});
